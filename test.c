87c82f43a9d24da2b09e9c8bc49e353386c71e2a
Runqiu_Ye <ye_runqiu@outlook.com>
2024-05-30 13:58:08 -0400

    update page up/down, home/end key

--- test.c
+++ test.c
@@ -430,12 +430,21 @@ void editorProcessKeypress() {
       break;
     }
     case END_KEY: {
-      E.cy = E.screencols - 1;
+      if (E.cy < E.numrows) {
+        E.cx = E.row[E.cy].size;
+      }
       break;
     }
     
     case PAGE_UP:
     case PAGE_DOWN: {
+      if (c == PAGE_UP) {
+        E.cy = E.rowoff;
+      } 
+      else if (c == PAGE_DOWN) {
+        E.cy = E.rowoff + E.screenrows - 1;
+        if (E.cy > E.numrows) E.cy = E.numrows;
+      }
       int times = E.screenrows;
       while (times > 0) {
         editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
