481c087f65487b766c148ff4ad17f98172ad2e67
Runqiu_Ye <ye_runqiu@outlook.com>
2024-06-09 22:26:29 -0400

    finished several functions for gapbuf

--- gapbuf.c
+++ gapbuf.c
@@ -14,3 +14,26 @@ bool is_gapbuf(gapbuf* gb) {
     if (strlen(gb->back) != gb->backlen) return false;
     return true;
 }
+
+bool gapbuf_at_left(gapbuf *gb) {
+    REQUIRES(is_gapbuf(gb));
+    return gb->frontlen == 0;
+}
+
+bool gapbuf_at_right(gapbuf *gb) {
+    REQUIRES(is_gapbuf(gb));
+    return gb->backlen == 0;
+}
+
+gapbuf* gapbuf_new() {
+    gapbuf* gb = xmalloc(sizeof(gapbuf));
+    gb->front = xcalloc(2, sizeof(char));
+    gb->back = xcalloc(2, sizeof(char));
+    gb->frontlen = 0;
+    gb->backlen = 0;
+    gb->frontlim = 1;
+    gb->backlim = 1;
+
+    ENSURES(is_gapbuf(gb));
+    return gb;
+}
