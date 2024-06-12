572396dc3404b1116535b244daa2d6d0d65257fe
Runqiu_Ye <ye_runqiu@outlook.com>
2024-06-11 23:28:37 -0400

    created main (editor itself)

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include "lib/contracts.h"
+#include "lib/xalloc.h"
+#include "gapbuf.h"
+#include "editor.h"
+#include "terminal.h"
+
+int main(void) {
+  editor* E = editor_new();
+  enableRawMode(E);
+
+  bool go = true;
+
+  while (go) {
+    char c;
+    read(STDIN_FILENO, &c, 1);
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } 
+    else {
+      printf("%d ('%c')\n", c, c);
+    }
+    if (c == 'q') go = false;
+  }
+
+  disableRawMode(E);
+  editor_free(E);
+  printf("Thanks for using RYe's editor");
+  return 0;
+}
