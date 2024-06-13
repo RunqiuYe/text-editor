944e98dae1f0733aad3b6ea76cf88ff1fe9ad5ee
Runqiu_Ye <ye_runqiu@outlook.com>
2024-06-12 22:21:13 -0400

    add open file

--- main.c
+++ main.c
@@ -10,10 +10,13 @@
 #include "editor.h"
 #include "terminal.h"
 
-int main(void) {
+int main(int argc, char* argv[]) {
   editor* E = editor_new();
   enableRawMode(E);
   setWindowSize(E);
+  if (argc >= 2) {
+    editor_open(E, argv[1]);
+  }
 
   bool* go = xcalloc(1, sizeof(bool));
   *go = true;
