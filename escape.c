a5ede5b8ecf878b5c0066aadf5f3ac008e05c08d
Runqiu_Ye <ye_runqiu@outlook.com>
2024-06-12 23:44:58 -0400

    update escape printing

--- escape.c
+++ escape.c
@@ -5,19 +5,21 @@
 #include <unistd.h>
 
 struct termios orig_termios;
-void disableRawMode() {
+void disableRawMode(void) {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
-void enableRawMode() {
+void enableRawMode(void) {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-int main() {  
+int main(void) {  
   enableRawMode();
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
